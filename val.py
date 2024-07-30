cd86ee4f6096c36580cd75f980050406ca07d9ec
NicolasSigl <nico.sigl@outlook.com>
2024-07-30 11:38:14 +0200

    cloned tensor for debugging

--- val.py
+++ val.py
@@ -130,7 +130,7 @@ def post_process_batch(data, imgs, paths, shapes, person_dets, kp_dets,
                     poses_mask = poses[mask]
 
                     if len(poses_mask):
-                        kpd[:, :4] = scale_coords(imgs[si].shape[1:], kpd[:, :4], shape)
+                        kpd[:, :4] = scale_coords(imgs[si].shape[1:], kpd[:, :4].clone(), shape) # cloned the tensor
                         kpd = kpd[:, :6].cpu()
 
                         for x1, y1, x2, y2, conf, cls in kpd:

2b6b6cf83e44b294f2ca6dc5195aba856cd4790d
NicolasSigl <nico.sigl@outlook.com>
2024-08-09 10:55:13 +0200

    fixed TypeError: dict_keys object is not subsriptable

--- utils/datasets.py
+++ utils/datasets.py
@@ -372,7 +372,15 @@ class LoadStreams:  # multiple IP or RTSP cameras
 
 
 def img2label_paths(img_paths, image_dir='images', labels_dir='labels'):
-    print(f"img2label_paths: ============= img_paths:\n {img_paths[:5]}\n ============= image_dir: {image_dir}\n=============== labels_dir: {labels_dir}")
+    try:
+        print(f"img2label_paths: ============= img_paths:\n {img_paths[:5]}\n ============= image_dir: {image_dir}\n=============== labels_dir: {labels_dir}")
+    except TypeError:
+        if type(img_paths) == 'dict':
+            img_paths.values()[:5]
+        else: 
+            print(f"can print img_paths of type: {type(img_paths)}")
+    
+    # adjust if human36m dataset
     if "human36m" in img_paths[0]:
         return [os.path.splitext(s.replace("human36m","human36m/" + labels_dir))[0] + '.txt' for s in img_paths]
 
